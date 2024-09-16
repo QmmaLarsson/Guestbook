349d04f666be9ae9c35baa379a62d98cc8ec6d4d
Emma Larsson <emma940112@gmail.com>
2024-09-16 04:20:30 +0200

    Lagt till funktionalitet som gör att användaren kan välja ett alternativ från menyn

--- Program.cs
+++ Program.cs
@@ -6,10 +6,45 @@ namespace GuestbookConsoleApp
     {
         public static void Main()
         {
+            while (true)
+            {
+                //Konsollen rensas innan den skrivs om
+                Console.Clear();
                 Console.WriteLine("EMMAS GÄSTBOK");
                 Console.WriteLine("1. Skriv i gästboken");
                 Console.WriteLine("2. Ta bort inlägg");
                 Console.WriteLine("X. Avsluta");
+
+                //Läser in användarens val
+                //Trim() tar bort eventuella mellanslag runt användarens inmatning
+                //ToUpper() Säkerställer att både "X" och "x" är giltiga val
+                string choice = Console.ReadLine().Trim().ToUpper();
+
+                //Konsollen rensas innan resultatet visas
+                Console.Clear();
+                //Switch-sats som hanterar användarens val
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Du har valt alternativ 1!");
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Du har valt alternativ 2!");
+                        break;
+
+                    case "X":
+                        return;
+
+                    default:
+                        Console.WriteLine("Error: Ogiltigt val");
+                        break;
+
+                }
+                //Vänta med att rensa konsollen
+                Console.WriteLine("Tryck på en tanget för att fortsätta.");
+                Console.ReadKey();
+            }
         }
     }
 }
